a50c47a65631a54c6056e56fe949f2d1ea324feb
centos <centos@localhost.localdomain>
2021-12-04 23:25:43 +0800

    摧毁线程工作完成

--- main.c
+++ main.c
@@ -11,13 +11,14 @@ int main()
     thread_start("k_thread_a", 31, func1, "argA ");
     thread_start("k_thread_b", 31, func2, "argB ");
     while (1)
-    {
+    {   
+        printf("main\n");
         schedule();
     }
 
     return 0;
 }
-
+// 打印a
 void func1(void *arg)
 {
     printf("func1\n");
@@ -28,7 +29,7 @@ void func1(void *arg)
         schedule();
     }
 }
-
+// 打印b
 void func2(void *arg)
 {
     char *para = arg;
--- uthread.c
+++ uthread.c
@@ -7,9 +7,8 @@ struct task_struct *main_thread;     // 主线程结构体
 struct list thread_ready_list;       // 就绪队列
 struct list thread_all_list;         //所有任务队列
 static struct list_elem *thread_tag; // 用于保存队列中的线程结点
-struct task_struct *cur_thread;            // 全局变量当前正在运行的线程
+struct task_struct *cur_thread;      // 全局变量当前正在运行的线程
 // 调用初始线程函数
-
 
 // 要实现一个switch_to 函数实现线程切换  // 在汇编函数中实现
 extern void switch_to(struct task_struct *cur, struct task_struct *next);
@@ -17,13 +16,12 @@ extern void switch_to(struct task_struct *cur, struct task_struct *next);
 static void make_main_thread(void);
 static void kernel_thread(thread_func *function, void *func_arg);
 
-
 static void kernel_thread(thread_func *function, void *func_arg)
 {
     function(func_arg);
     // 结束会回到这个位置 在这里可以摧毁当前线程
     printf("thread_destory\n");
-    schedule();
+    thread_exit(running_thread(), true);
 }
 // 获取当前正在运行的线程结构体指针
 struct task_struct *running_thread()
@@ -55,9 +53,9 @@ void init_thread(struct task_struct *pthread, char *name, int prio)
     pthread->stack_magic = 0x20001221;
 }
 
-
 // 创建线程
-void thread_create(struct task_struct* pthread, thread_func function, void* func_arg) {
+void thread_create(struct task_struct *pthread, thread_func function, void *func_arg)
+{
     /* 再留出线程栈的空间 */
     pthread->self_stack -= sizeof(struct thread_stack);
     /* 正式开始初始化 */
@@ -89,7 +87,6 @@ struct task_struct *thread_start(char *name, int prio, thread_func function, voi
     return thread;
 }
 
-
 /* 将main函数线程也就是主线程完善结构体 */
 static void make_main_thread(void)
 {
@@ -98,7 +95,7 @@ static void make_main_thread(void)
     init_thread(main_thread, "main", 31);
     // main 函数是当前线程 不在thread_ready_list中
     assert(!elem_find(&thread_all_list, &main_thread->all_list_tag));
-    
+
     list_append(&thread_all_list, &main_thread->all_list_tag);
 }
 // 线程库初始化
@@ -115,7 +112,7 @@ void thread_init(void)
 void schedule()
 {
     // 获取当前正在运行的线程
-    struct task_struct* cur = running_thread();
+    struct task_struct *cur = running_thread();
     // 添加进就绪队列尾部
     list_append(&thread_ready_list, &cur->general_tag);
     // 修改线程状态
@@ -136,10 +133,29 @@ void schedule()
 }
 // 退出线程 回收线程需要的资源 改变线程的状态
 // 将其退出调度队列
-void thread_exit(struct task_struct* thread_over, bool need_schedule) 
-{  
+void thread_exit(struct task_struct *thread_over, bool need_schedule)
+{
+    printf("thread_exit_start\n");
     // 改变线程的状态
+    thread_over->status = TASK_DIED; //线程死亡
     // 将其从调度队列中移除
+    list_remove(&thread_over->general_tag);
     // 将其从总线程队列中移除 //释放线程结构体在其他线程中进行
-    // 进行最后一次调度
+    list_remove(&thread_over->all_list_tag);
+    //printf("thread_exit_over\n");
+
+
+    // 进行最后一次调度 // 此处不能采用调度器
+    thread_tag = NULL; // thread_tag清空                   
+    /* 将thread_ready_list队列中的第一个就绪线程弹出,准备将其调度上cpu. */
+    // 从就绪队列中取出一个
+    thread_tag = list_pop(&thread_ready_list);
+    // 获取首地址
+    struct task_struct *next = elem2enter(struct task_struct, general_tag, thread_tag);
+    // 修改状态
+    next->status = TASK_RUNNING;
+    //更改储存的cur
+    cur_thread = next;
+    switch_to(thread_over, next);
+
 }
